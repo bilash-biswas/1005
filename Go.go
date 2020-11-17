ff975d8364e7f82f6c96365e5eba656bfae7e4bf
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:09:42 +0600

    Update and rename Go to Go.go

--- Go.go
+++ Go.go
@@ -7,6 +7,6 @@ func main() {
 	var num2 float64
 	fmt.Scanln(&num1)
 	fmt.Scanln(&num2)
-	var result float64 = (num1*3.5 + num2*7.5) / 11
+	var result float64 = (num1 * 3.5 + num2 * 7.5) / 11
 	fmt.Printf("MEDIA = %.5f\n", result)
 }
