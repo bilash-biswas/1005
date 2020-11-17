3a19891cfb6c0987b30c4e50f8457e637294ae7e
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:10:01 +0600

    Update and rename Dart to Dart.dart

--- Dart.dart
+++ Dart.dart
@@ -4,7 +4,7 @@ void main(){
 
   num1 = double.parse(stdin.readLineSync());
   num2 = double.parse(stdin.readLineSync());
-  double mul = ((num1 * 3.5) + (num2 * 7.5))/11;
+  double mul = ((num1 * 3.5) + (num2 * 7.5)) / 11;
   var num = mul.toStringAsFixed(5);
   print("MEDIA = $num");
 }
