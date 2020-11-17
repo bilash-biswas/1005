cf65843d0757d6ff59692d2fdc622ab8532d33c0
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:08:56 +0600

    Update and rename JavaScript to JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,5 +3,5 @@ var lines = input.split('\n');
 
 var A = parseFloat(lines.shift());
 var B = parseFloat(lines.shift());
-var C = (A*3.5 + B*7.5)/11;
+var C = (A * 3.5 + B * 7.5) / 11;
 console.log("MEDIA = " + C.toFixed(5));
