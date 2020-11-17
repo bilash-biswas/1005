1757ddc2d14c06ffa1ca698848f3c182dfb274c0
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:07:28 +0600

    Update and rename PHP to PHP.php

--- PHP.php
+++ PHP.php
@@ -2,8 +2,7 @@
 
 $num1 = (float)readline();
 $num2 = (float)readline();
-$mul = ($num1 * 3.5 + $num2 * 7.5)/11.0;
+$mul = ($num1 * 3.5 + $num2 * 7.5) / 11.0;
 echo "MEDIA = ".number_format($mul,5,'.','')."\n";
-
 
 ?>
